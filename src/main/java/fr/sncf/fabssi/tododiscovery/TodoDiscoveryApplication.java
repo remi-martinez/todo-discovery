9974fed25cf58d4e95693da42c44a3dec8310a27
remi-martinez <remi.martinez74500@outlook.fr>
2020-09-29 17:00:41 +0200

    Initial commit

---
+++ src/main/java/fr/sncf/fabssi/tododiscovery/TodoDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package fr.sncf.fabssi.tododiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sncf/fabssi/tododiscovery/TodoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sncf.fabssi.tododiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
